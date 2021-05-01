9abc9848635ba487d590f1973e62f4ea5ecbc75d
dgadacha <dylangadacha@yahoo.fr>
2021-05-01 14:55:56 +1100

    munanyo sound
    munanyo sound

--- views/js/script.js
+++ views/js/script.js
@@ -1,2 +1,5 @@
-var audio = new Audio('https://www.myinstants.com/media/sounds/nouveau-jingle-netflix.mp3');
+var netflix = '/nouveau-jingle-netflix.mp3';
+var munanyo = '/chupapi-short.mp3';
+
+var audio = new Audio('https://www.myinstants.com/media/sounds/'+netflix);
 audio.play();
